d654d78322fb89553471ead95c8d6278a152ed11
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2024-04-14 20:09:51 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Config.Services;
 using GameLib.CharacterFactory.Services;
 using GameLib.Lootsystem.Models;
 using GameLib.Lootsystem.Services;
+using System.Collections.Generic;
+using System.Linq; // Added for LINQ
 
 namespace YourNamespace
 {
@@ -16,7 +18,6 @@ namespace YourNamespace
         static void Main(string[] args)
         {
             // Give the path to the log files
-            //string logDirectory = "/home/tito/Projects/EngineGame/YamlLog"; // Linux
             string logDirectory = "C:\\Users\\mads-\\OneDrive\\Skrivebord\\AWSC\\GameEngine\\YamlLog\\"; // Windows
 
             Console.WriteLine("Hello and welcome to the game!");
@@ -39,8 +40,8 @@ namespace YourNamespace
             LootSystem lootSystem = new LootSystem();
 
             // Create some weapons
-            IItem sword = new Weapon("Sword");
-            IItem axe = new Weapon("Axe");
+            IItem sword = new Weapon("Sword", 50); // Added value for demonstration
+            IItem axe = new Weapon("Axe", 40); // Added value for demonstration
 
             // Add weapons to bags
             Bag heroBag = new Bag();
@@ -59,6 +60,9 @@ namespace YourNamespace
 
             // Temporary game service until we move it
             GameService gameService = new GameService(world, hero, monster, logger);
+
+            // Test LINQ methods
+            TestLINQ(heroBag, monsterBag);
 
             while (true)
             {
@@ -83,6 +87,40 @@ namespace YourNamespace
             Console.SetCursorPosition(character.X * 3 + 1, character.Y + 1); // Adjust position for spacing
             Console.ForegroundColor = ConsoleColor.White; // Character color
             Console.Write(character is Player ? 'H' : 'M'); // 'H' for hero, 'M' for monster
+        }
+
+        static void TestLINQ(Bag heroBag, Bag monsterBag)
+        {
+            // Test LINQ methods for querying items in the bag
+            Console.WriteLine("Items in hero's bag:");
+            foreach (var item in heroBag.GetItems())
+            {
+                Console.WriteLine($"Name: {item.Name}, Value: {((Weapon)item).Value}"); // Casting to Weapon for demonstration
+            }
+
+            // Test LINQ methods for querying bags in the loot system
+            List<Bag> bags = new List<Bag> { heroBag, monsterBag };
+
+            Console.WriteLine("\nBags containing a Sword:");
+            var bagsWithSword = bags.Where(bag => bag.GetItems().Any(item => item.Name == "Sword"));
+            foreach (var bag in bagsWithSword)
+            {
+                Console.WriteLine($"Bag contains a Sword: {bag.GetItems().Any(item => item.Name == "Sword")}");
+            }
+
+            Console.WriteLine("\nBag with the highest total value:");
+            var bagWithHighestValue = bags.OrderByDescending(bag => bag.GetTotalValue()).FirstOrDefault();
+            if (bagWithHighestValue != null)
+            {
+                Console.WriteLine($"Bag with the highest value: {bagWithHighestValue.GetTotalValue()}");
+            }
+
+            Console.WriteLine("\nBag with the most items:");
+            var bagWithMostItems = bags.OrderByDescending(bag => bag.GetItems().Count()).FirstOrDefault();
+            if (bagWithMostItems != null)
+            {
+                Console.WriteLine($"Bag with the most items: {bagWithMostItems.GetItems().Count()}");
+            }
         }
     }
 }
